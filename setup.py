f5e03894f4e8bd29a394b67f5a2c1e401ea00c67
axiomcura <erikishere3@gmail.com>
2022-09-21 22:14:43 -0600

    added setup.py file for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    author="Erik Serrano",
+    description="Gene reads CLI Tool",
+    version="0.1",
+    python_requires=">3.10",
+    packages=find_packages(),
+    install_requires=["matplotlib"],
+    scripts=["plot_gtex.py"],
+)
