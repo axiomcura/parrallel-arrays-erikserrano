24e2eecf62146b2afb3bace4f9b24049a6df178d
axiomcura <erikishere3@gmail.com>
2022-10-06 14:44:24 -0600

    functional test added

---
+++ test_search.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run linear_search python search.py -i toydata/lorem_ipsum_data.txt -t dolor -a linear
+assert_in_stdout 2
+assert_exit_code 0
+
+run binary_search python search.py -i toydata/lorem_ipsum_data.txt -t dolor -a binary
+assert_in_stdout 2
+assert_exit_code 0
+
+run check_styling pycodestyle *.py tests/*.py
+assert_exit_code 0
