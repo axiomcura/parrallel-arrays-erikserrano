f29d1bb94b64baf72a741667a8b484b862ebc5da
axiomcura <erikishere3@gmail.com>
2022-10-11 09:10:54 -0600

    added functional and unit test directory

---
+++ tests/functional/run_func_test.sh
@@ -0,0 +1,10 @@
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run linear_search python ../../search.py -i ../../toydata/lorem_ipsum_data.txt -t dolor -a linear
+assert_in_stdout 2
+assert_exit_code 0
+
+run binary_search python ../../search.py -i ../../toydata/lorem_ipsum_data.txt -t dolor -a linear
+assert_in_stdout 2
+assert_exit_code 0
