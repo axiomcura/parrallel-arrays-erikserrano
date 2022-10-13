bbb23bec66ed8020c8f8e3d6fb9d8b39f4c10c34
axiomcura <erikishere3@gmail.com>
2022-10-13 16:04:43 -0600

    edit functional testing

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ set -e
 
 # downloading data
 echo "Downloading required datasets"
-wget https://github.com/swe4s/lectures/raw/master/data_integration/gtex/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz
-wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
+wget https://github.com/swe4s/lectures/raw/master/data_integration/gtex/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz -O GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz
+wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt -O GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
 
 # running functional, unit and styling tests
 echo "Running functional, unit, and styling tests"
--- tests/functional/run_func_test.sh
+++ tests/functional/run_func_test.sh
@@ -1,6 +1,9 @@
 test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
 . ssshtest
 
+wget https://github.com/swe4s/lectures/raw/master/data_integration/gtex/GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz -O GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz
+wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt -O GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
+
 run linear_search python ../../search.py -i ../../toydata/lorem_ipsum_data.txt -t dolor -a linear
 assert_in_stdout 2
 assert_exit_code 0
@@ -10,7 +13,7 @@ assert_in_stdout 2
 assert_exit_code 0
 
 # running plot_gtex
-run plot_gtex plot_gtex.py \
+run plot_gtex ../../plot_gtex.py \
 --gene_reads GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz \
 --sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt \
 --gene ACTA2 \
