ca56d8715e8a712ecb677776b9d0208418c748bb
axiomcura <erikishere3@gmail.com>
2022-10-13 15:49:18 -0600

    added functional tests

--- test_search.sh
+++ test_search.sh
@@ -12,3 +12,14 @@ assert_exit_code 0
 
 run check_styling pycodestyle *.py tests/unit/*.py
 assert_exit_code 0
+
+run plot_gtex plot_gtex.py \
+--gene_reads GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz \
+--sample_attributes GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt \
+--gene ACTA2 \
+--output_file ACTA2.png \
+--fig_width 12 \
+--fig_height 5 \
+--threshold 200000
+assert_in_stdout 2
+assert_exit_code 0
